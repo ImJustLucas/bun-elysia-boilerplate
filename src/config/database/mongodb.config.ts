582df26ce464c13eaca3238879f305ccb8bb0627
Lucas <bellierlucas.pro@gmail.com>
2023-10-17 10:22:57 +0200

    🚑 fix listen port too early

--- src/config/database/mongodb.config.ts
+++ src/config/database/mongodb.config.ts
@@ -5,4 +5,9 @@ const mongoDBURI = process.env.MONGODB_URI ?? "mongodb://localhost:27017";
 
 mongoose.connect(mongoDBURI);
 
+mongoose.connection.on("connected", () => {
+  //Write a beautiful log message
+  console.log("Connection to MongoDB established successfully");
+});
+
 export default mongoose;
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ import "@config/database/mongodb.config";
 import { auth } from "@auth/auth.controller";
 import { apiRoutes } from "@api/index";
 
-const api = new Elysia().listen(process.env.PORT || 8080);
+const api = new Elysia();
 
 api.use(auth);
 
 api.use(apiRoutes);
+
+api.get("/", () => "Welcome to Elysia!");
+
+api.listen(process.env.PORT || 8080);
 
 console.log(
   `🦊 Elysia is running at ${api.server?.hostname}:${process.env.PORT || 8080}`
